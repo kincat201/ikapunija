c45c93dfc4dcbfc1860ca9d00d41e86a51776971
yohanwave <yohan@wvi.co.id>
2021-03-21 22:07:29 +0700

    bug on sql update

--- web_ikapunija/database/updates/21032021.sql
+++ web_ikapunija/database/updates/21032021.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `user_alumni`
-	ADD COLUMN `province_id` INT(10) NOT NULL AFTER `negara_other`,
+	ADD COLUMN `province_id` INT(10) NOT NULL AFTER `negara_id`,
 	ADD COLUMN `city_id` INT(10) NOT NULL AFTER `province_id`,
 	ADD COLUMN `city_other` VARCHAR(200) NOT NULL DEFAULT '' AFTER `city_id`,
 	ADD COLUMN `company` VARCHAR(200) NULL DEFAULT NULL AFTER `nik`,
